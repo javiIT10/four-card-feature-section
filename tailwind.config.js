23a977ee9a94ede6ec172327956af19bcd57280d
Javier <javier.duarte.iti@gmail.com>
2024-07-31 08:12:46 -0500

    Four Card Feature Section - 1.0

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}", "./src/js/app.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        /* ### Primary */
+        red: "hsl(0, 78%, 62%)",
+        cyan: "hsl(180, 62%, 55%)",
+        orange: "hsl(34, 97%, 64%)",
+        blue: "hsl(212, 86%, 64%)",
+
+        /* ### Neutral */
+        "very-dark-blue": "hsl(234, 12%, 34%)",
+        "grayish-blue": "hsl(229, 6%, 66%)",
+        "very-light-gray": "hsl(0, 0%, 98%)",
+      },
+    },
+  },
+  plugins: [],
+};
